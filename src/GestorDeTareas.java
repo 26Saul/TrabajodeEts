6e97a5d2852be40d7eaffbab823fd4bdf515ba68
kacper <kacperok201@gmail.com>
2026-03-04 18:38:29 +0000

    subida de cambios para pasar a develop

--- src/GestorDeTareas.java
+++ src/GestorDeTareas.java
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-public class GestorDeTareas {
 
-    public void mostrarTareas(){
-        if(contador == 0){
-            System.out.println("No hay tareas para mostrar.");
-        } else {
-            System.out.println("\n Lista de tareas:");
-            for(int i = 0; i < contador; i++){
-                System.out.println((i + 1) + ". " + tareas[i]);
-            }
-        }
-    }
-}
-=======
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +19,16 @@ public class GestorDeTareas {
     public List<Tarea> getTareas() {
         return tareas;
     }
+    public void mostrarTareas(){
+        if(contador == 0){
+            System.out.println("No hay tareas para mostrar.");
+        } else {
+            System.out.println("\n Lista de tareas:");
+            for(int i = 0; i < contador; i++){
+                System.out.println((i + 1) + ". " + tareas[i]);
+            }
+        }
+    }
 
 }
->>>>>>> 4cc4005e5e23d3819a67dabae407cb2dbf684f8d
+
